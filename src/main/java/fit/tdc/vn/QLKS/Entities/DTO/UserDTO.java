3c412c0e2cc12fef75070a1d14ef9a08831f2df7
taibeo <donguyentai05@gmail.com>
2025-05-17 20:48:00 +0700

    yyyy

---
+++ src/main/java/fit/tdc/vn/QLKS/Entities/DTO/UserDTO.java
@@ -0,0 +1,42 @@
+package fit.tdc.vn.QLKS.Entities.DTO;
+
+public class UserDTO {
+	private String password;
+	private String phone;
+	private String email;
+	private String fullName;
+	private String cccd;
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getFullName() {
+		return fullName;
+	}
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+	public String getCccd() {
+		return cccd;
+	}
+	public void setCccd(String cccd) {
+		this.cccd = cccd;
+	}
+	
+	
+
+}
